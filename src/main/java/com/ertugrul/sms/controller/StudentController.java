8d26c7df2b5b9c307eae28d6aa12c811854fd267
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-07-02 19:46:10 +0300

    student controller is added

---
+++ src/main/java/com/ertugrul/sms/controller/StudentController.java
@@ -0,0 +1,10 @@
+package com.ertugrul.sms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/students")
+public class StudentController {
+
+}
